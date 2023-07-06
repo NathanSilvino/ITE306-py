2b06c5d9b6b952a8e8dc7d9c5a366c9072d160ce
NathanSilvino <114991093+NathanSilvino@users.noreply.github.com>
2023-07-05 22:44:03 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+print("Programmer : NATHANIEL S SILVINO")
+print("Date Created : 07/06/23")
+print("Description of the Programmer : POGI ")
+print("First Name : NATHANIEL")
+print("Middle Name : SANTIAGO")
+print("last Name :SILVINO")
+print("Course : BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY MAJOR IN WEV DEV")
+print("Year : 3rd YEAR COLLEGE")
+print("Gender : MALE")
+print("Civil Status : SINGLE")
+print("Nationality : FILIPINO")
+
+print("MOTTO : ALL GOOD IN THE HOOD")
+
+print("School : PHINMA Araullo University")
+print("College : CITE")
+
+num1 = 5
+num2 = 8
+
+sum = num1 + num2
+print("SUM = ",sum)
+
+diff = num1 - num2
+print("DIFFERENCE = ",diff)
+
+prod = num1 * num2
+print("PRODUCT = ",prod)
+
+qout = num1 / num2
+print("QOUTIENT = ",qout)
+
